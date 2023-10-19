bf84fcc3f0ad80b516656fb54737612f56a6ac27
Héctor Sandoval <ingsan@live.com.mx>
2023-10-19 12:49:24 -0600

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Héctor Manuel Sandoval Landázuri on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
